c711fc7b96416616cfde0f06780eb0a6c7e84436
Panagiotis Triantafyllos <ptriantap@gmail.com>
2024-02-15 10:25:40 +0200

    first commit: project initialization

---
+++ src/main/java/com/pluralsight/springboot3fundamentals/SpringBoot3FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springboot3fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3FundamentalsApplication.class, args);
+	}
+
+}
